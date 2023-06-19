adcef015323a62d507cdad73c45afcde05c8f384
Vishnupriyadhanapal <115882330+Vishnu-06-Priya@users.noreply.github.com>
2023-06-19 20:38:59 +0530

    Add files via upload

---
+++ src/content_option.js
@@ -0,0 +1,127 @@
+const logotext = "VISHNUPRIYA";
+const meta = {
+    title: "vishnupriya",
+    description: "I’m vishnupriya data Full stack devloper,completed my internship in Guvi Geek private limited",
+};
+
+const introdata = {
+    title: "I’m vishnupriya Dhanapal",
+    animated: {
+        first: "Full Stack Developer",
+        second: "I code cool Websites",
+    },
+    description: "A beginner level programmer,who possess certification on MERN stack development.Is awating for oppurtunity as a Full stack developer ",
+    your_img_url: "https://images.unsplash.com/photo-1491975474562-1f4e30bc9468?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZGV2ZWxvcGVyJTIwaW1hZ2UlMjBmb3IlMjBnaXJsc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60",
+};
+
+const dataabout = {
+    title: "About Myself",
+    aboutme: " Fresher as a fullstack developer,I am eagerly awaiting for my oppurtunity.To explore my stored knowledges on the upcoming projects with my hiring team.Experienced a full stack project using MERN stack during My Internship at Guvi geek private limited .",
+};
+const worktimeline = [{
+        jobtitle: "Student Internship in Full Stack Development",
+        where: "Guvi Geek Private Limited",
+        year: "2023",
+    },
+    
+];
+
+const skills = [{
+        name: "Javascript",
+        value: 85,
+    },
+    {
+        name: "Html and css",
+        value: 85,
+    },
+    {
+        name: "Reactjs",
+        value: 90,
+    },
+    {
+        name: "NodeJs",
+        value: 80,
+    },
+    {
+        name: "ExpressJs",
+        value: 80,
+    },
+    {
+        name: "MongoDB",
+        value: 80,
+    },
+    {
+        name: "Mysql",
+        value: 80,
+    },
+];
+
+const services = [{
+        title: "Frontend developement",
+        description: "Having a complete knowledge about frontend with ReactJs and created 'A Pokemon API' as a mini Project .",
+    },
+    { 
+        title: "Backend development",
+        description: "Having a complete knowledge about backend with NodeJs and created 'Equipment hiring portal' as a mini Project.",
+    },
+    {
+        title: "Full Stack-MERN Stack development",
+        description: "Having a complete knowledge about MERN Stack with Creative ideas and created 'Food Delivery App' as a complete Full Stack Project  .",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "vishnupriyadhanapal6899@gmail.com",
+    YOUR_PHONE: "9994734372",
+    description: "I hereby declare to the hiring team,that I have done my portfolio upto my knowledge and awaiting for the perfect oppurtunity to explore myself.Please do contact me for a enthusiastic team member to your eminent team. . ",
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
